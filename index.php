7c130192c79c376db8393f399ffe3afd384a996b
indhhdesintia <indhhdesintia@gmail.com>
2026-03-30 13:55:59 +0800

    first commit

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ session_start();
                     <i class="fas fa-fw fa-star"></i>
                     <span>Ulasan</span>
                 </a>
-                
+                     
             
                 
             </li>
@@ -335,8 +335,8 @@ session_start();
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Earnings (Monthly)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                                Earnings</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
                                         </div>
                                         <div class="col-auto">
                                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
